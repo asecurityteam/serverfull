164f2aca45c1c7f8911979b8e6eb91be604abcf2
Kevin Conway <kconway@atlassian.com>
2019-04-26 07:25:45 -0500

    Migrate the static fetcher to top-level
    This patch moves the static fetcher to the top-level package and
    conforms it to the new domain types of Function and Fetcher.

---
+++ staticfetcher.go
@@ -0,0 +1,34 @@
+package serverfull
+
+import (
+	"context"
+)
+
+// Static is an implementation of the Fetcher that maintains a static mapping
+// of names to Function instances. This implementation is a highly simplified form
+// for the purposes of reducing risk in operations. Notably, runtimes that leverage
+// this implementation do not need to perform any orchestration of external systems
+// as all invocations of the Functions happen within the process and share the
+// runtime's resources. Additionally, there is no "live update" feature which means
+// there are less moving parts that might fail when attempting to start or update a
+// Function.
+//
+// The trade-off is that updates to, additions of, and removals of Functions must be
+// accomplished by generating a new build and redeploying the runtime. There are no
+// options for updating or adding in-place. Operators and developers who choose this
+// feature must take care that redeployments of the system do not cause downtime as
+// all Functions will be affected together.
+type Static struct {
+	// Functions is the underlying static map of function names to executable
+	// functions. The keys of the map will be used as the name of the Function.
+	Functions map[string]Function
+}
+
+// Fetch resolves the name using the internal mapping.
+func (f *Static) Fetch(ctx context.Context, name string) (Function, error) {
+	h, ok := f.Functions[name]
+	if !ok {
+		return nil, NotFoundError{ID: name}
+	}
+	return h, nil
+}
---
+++ staticfetcher_test.go
@@ -0,0 +1,73 @@
+package serverfull
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+)
+
+func TestStatic_Fetch(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	found := NewMockFunction(ctrl)
+
+	type fields struct {
+		Functions map[string]Function
+	}
+	type args struct {
+		ctx  context.Context
+		name string
+	}
+	tests := []struct {
+		name        string
+		fields      fields
+		args        args
+		want        Function
+		wantErr     bool
+		wantErrType reflect.Type
+	}{
+		{
+			name:        "missing function",
+			fields:      fields{Functions: make(map[string]Function)},
+			args:        args{ctx: context.Background(), name: "missing"},
+			want:        nil,
+			wantErr:     true,
+			wantErrType: reflect.TypeOf(NotFoundError{}),
+		},
+		{
+			name: "found function",
+			fields: fields{
+				Functions: map[string]Function{
+					"found": found,
+				},
+			},
+			args:    args{ctx: context.Background(), name: "found"},
+			want:    found,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &Static{
+				Functions: tt.fields.Functions,
+			}
+			got, err := f.Fetch(tt.args.ctx, tt.args.name)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Static.Fetch() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if (err != nil) && tt.wantErr && tt.wantErrType != nil {
+				errType := reflect.TypeOf(err)
+				if errType != tt.wantErrType {
+					t.Errorf("Static.Fetch() error = %v, wantErrType %v", errType, tt.wantErrType)
+				}
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Static.Fetch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
